0c221a6482a7c42f0ac80855e549b2babc0e1029
Mariana Arce <mar_ana22@hotmail.com>
2025-07-24 00:26:51 +0000

    Apy_Python_Mariana

--- src/app.py
+++ src/app.py
@@ -6,5 +6,31 @@ from dotenv import load_dotenv
 # load the .env file variables
 load_dotenv()
 
+# Get credential values
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
 
-# Spotify API credentials
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+spotify = spotipy.Spotify(auth_manager=auth_manager)
+
+# 2fZnIGcmeZuhplsTcCBmvD Cocofunka
+
+#Obtener las mejores canciones del artista
+results= spotify.artist_top_tracks("2fZnIGcmeZuhplsTcCBmvD") 
+
+songs= []
+
+for track in results ['tracks']:
+    songs.append({"name":track['name'],
+                  "popularity":track['popularity'],
+                  "duracion_min":track['duration_ms']/60000})
+    
+print(songs)
+
+df = pd.DataFrame(songs)
+
+print(df)
+
